a8919bf36b5bb3c7061892edd9ea7147098105e4
tuan <rickytuan1991@gmail.com>
2022-10-24 07:05:16 +0300

    done

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 // import engin from express-handlebars
-const { engine } = require('express-handlebars');
+const hbs = require('express-handlebars')
 // import morgan vs use morgan HTTP logger
 const morgan = require('morgan')
 app.use(morgan('tiny'))
 // use express-handlebars
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
 
+app.use(express.static('../public'))
 app.get('/', (req, res) => {
-    // res.status(200).send(`<h1 style='red'>hello world</h1>`)
-    res.render('home');
+    // res.sendFile(path.resolve(__dirname, './resources/public/index.html'))
+})
+app.all('*', (req, res) => {
+    res.status(404).send('resource not found')
+})
 
-}).listen(3000, () => {
+app.listen(3000, () => {
     console.log("listening port 3000 ....")
 })
