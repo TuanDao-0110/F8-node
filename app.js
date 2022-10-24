c90d0753bea2bffc284d9b9427fff74235af192b
tuan <rickytuan1991@gmail.com>
2022-10-24 04:35:51 +0300

    i express handlebars

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     let a = 2
     let b = 2
     let c = 3
-    res.status(200).send('hello ')
+    res.status(200).send(`<h1 style='red'>hello world</h1>`)
 
 }).listen(3000, () => {
     console.log("listening port 3000 ....")
