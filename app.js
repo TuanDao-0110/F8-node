21cbdc02bb5b6b63642ca8493c03537cde15b053
tuan <rickytuan1991@gmail.com>
2022-10-24 04:28:57 +0300

    done

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+morgan('dev')
 
+// app.use(morgan)
 
 app.get('/', (req, res) => {
+    let a = 2
+    let b = 2
+    let c = 3
     res.status(200).send('hello ')
 
 }).listen(3000, () => {
